037d9c1943b9b409fbea0218ba801fa7a4289f0c
GZennurov <zwgybds@yahoo.com>
2019-08-15 17:04:41 +0300

    project initialized

---
+++ pages/home/Home.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <div class="elem-test">
+            {{testCaption}}
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+
+import { Vue, Component, Prop } from "vue-property-decorator";
+
+@Component
+export default class Layout extends Vue {
+    testCaption: string = "test";
+}
+</script>
+
+<style>
+
+.elem-test {
+    height: 50px;
+    width: 50px;
+    background-color: blanchedalmond;
+}
+
+</style>
