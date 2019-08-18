229aea4441355beb8459592efd3e5bc5a2a7ceae
GZennurov <zwgybds@yahoo.com>
2019-08-18 22:49:42 +0300

    nav menu developed, fonts's been set

--- src/pages/Layout.vue
+++ src/pages/Layout.vue
@@ -3,16 +3,19 @@
         <header>
             <nav class="nav">
                 <ul class="nav__list">
-                    <li class="nav__list-item" style="width: 25%;">
+                    <li class="nav__list-item" style="width: 20%;">
                         главная
                     </li>
-                    <li class="nav__list-item" style="width: 25%;">
+                    <li class="nav__list-item" style="width: 20%;">
                         фото и видео
                     </li>
-                    <li class="nav__list-item" style="width: 25%;">
+                    <li class="nav__list-item" style="width: 20%;">
+                        <div class="nav__list-logo"></div>
+                    </li>
+                    <li class="nav__list-item" style="width: 20%;">
                         форма отзыва
                     </li>
-                    <li class="nav__list-item" style="width: 25%;">
+                    <li class="nav__list-item" style="width: 20%;">
                         html academy
                     </li>
                 </ul>
@@ -40,10 +43,13 @@ export default class Layout extends Vue {
 <style>
 
 .layout {
+    background-color: white;
     width: 1150px;
     margin: 0 auto;
+    padding: 0 25px;
     display: flex;
     flex-direction: column;
+    --text-size__middle: 18px;
 }
 
 main {
@@ -57,7 +63,27 @@ main {
 .nav__list {
     list-style: none;
     display: flex;
+    margin: 0;
+    padding: 0;
+    width: 100%;
+}
+
+.nav__list-item {
+    position: relative;
+    display: flex;
+    justify-content: center;
+    text-transform: uppercase;
+    font-weight: bold;
+    font-size: var(--text-size__middle);
     margin: 25px 0px;
+    width: 186px;
+}
+
+.nav__list-logo {
+    content: url(".././assets/logotypedesktopsvg.png");
+    z-index: 1;
+    position: absolute;
+    top: 0;
 }
 
 </style>
