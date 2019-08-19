91ce3354e7c7b249c17ed9f1f68d9e67b5916a04
GZennurov <zwgybds@yahoo.com>
2019-08-19 18:07:29 +0300

    Pages header and footer developed. Started developing sedona photos page.

---
+++ src/pages/sedona-photo/components/SightPhoto.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="sight-photo">
+        <img class="sight-photo__img" :src="sightPic.photoSrc"/>
+        <div class="sight-photo__info">
+            <div class="sight-photo__info-name">
+                неродные просторы
+            </div>
+        </div>
+    </div>
+    
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from "vue-property-decorator";
+import { ISightPic } from "@/service/Sight";
+
+@Component
+export default class SightPhoto extends Vue{
+    @Prop({ default: null }) sightPic: ISightPic;  
+
+}
+
+</script>
+
+<style>
+
+.sight-photo {
+    position: relative;
+}
+
+.sight-photo__img {
+    width: 100%;
+    object-fit: cover;    
+}
+
+.sight-photo__info {
+    display: flex;
+    align-items: center;
+    position: absolute;
+    bottom: 0;
+    background-color: black;
+    opacity: 0.3;
+    width: 100%;
+    height: 70px;
+}
+
+.sight-photo__info-name {
+    color: white;
+    opacity: 1;
+}
+
+</style>
---
+++ src/service/Sight.ts
@@ -0,0 +1,9 @@
+interface ISightPic {
+    sightName: string;
+    photoSrc: string;
+    author: string;
+    likesCount: number; 
+    // incLikes(): void;
+}
+
+export { ISightPic };
