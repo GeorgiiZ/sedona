0ec719a5d1d7a50f26afc09d08dd4cc99e2ff4b5
GZennurov <zwgybds@yahoo.com>
2019-09-19 10:55:14 +0300

    added scss option

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,18 @@
+const path = require('path');
+
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
       ? 'sedona'
-      : ''
+      : '',
+
+    pluginOptions: {
+        'style-resources-loader': {
+            preProcessor: 'scss',
+            patterns: [
+                path.resolve(__dirname, './src/assets/styles/global.scss')
+            ]
+        }
+    }
   }
+
+  
