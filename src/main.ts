3c6adabd4ef668cdc51c634b923eee3e0e1d6386
georgiy.zennurov <georgiy.zennurov@south.rt.ru>
2020-04-23 15:12:16 +0300

    4-th change to see in host module

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,7 @@
 import Vue from 'vue';
 import App from './App.vue';
 import Router from './router/router';
-
 Vue.config.productionTip = false;
-
 new Vue({
   render: (h) => h(App),
   router: Router,
