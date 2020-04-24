5abf1c8c76132df96874185368d6b9735bcf736c
georgiy.zennurov <georgiy.zennurov@south.rt.ru>
2020-04-24 08:33:16 +0300

    Checnged main.ts file

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,16 @@ import App from './App.vue';
 import Router from './router/router';
 Vue.config.productionTip = false;
 
+
+
+
+
 new Vue({
   render: (h) => h(App),
   router: Router,
 }).$mount('#app');
+
+
+
+
 
